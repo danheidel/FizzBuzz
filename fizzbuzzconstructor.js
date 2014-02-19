add4420686db0780696418eace713e5494ef11fd
Dan Heidel <dan.heidel@gmail.com>
2014-02-18 22:03:57 -0800

    fixed stupid return val mistake in fbconstructor

--- fizzbuzzconstructor.js
+++ fizzbuzzconstructor.js
@@ -1,35 +1,36 @@
 'use strict';
 
 var FizzBuzzConstructor = function(iList){
-  var _list = [];
-  var Constructor = function(){
-    //this._list = [];
-    var convNode = function(iNode){
-      //converts incoming key/val pair into individual key/val pairs in an object
-      //'key' is converted to a numeric to increase comparison speed later
-      var tempNode = {'key': +Object.keys(iNode)[0], 'val': iNode[Object.keys(iNode)[0]]};
-      return tempNode;
-    };
-    var parseList = function(iList){
-      //iList is an array w/ key/value pairs of the modulo values/label strings
-      //iList is then parsed into an array of objects into list
-      _list = [];
-      for(var rep=0;rep<iList.length;rep++){
-        _list.push(convNode(iList[rep]));
-      }
-    };
-    _list = parseList(iList || []);
+  var convNode = function(iNode){
+    //converts incoming key/val pair into individual key/val pairs in an object
+    //'key' is converted to a numeric to increase comparison speed later
+    var tempNode = {'key': +Object.keys(iNode)[0], 'val': iNode[Object.keys(iNode)[0]]};
+    return tempNode;
   };
+  var parseList = function(iList){
+    //iList is an array w/ key/value pairs of the modulo values/label strings
+    //iList is then parsed into an array of objects into list
+    _list = [];
+    console.log(_list);
+    for(var rep=0;rep<iList.length;rep++){
+      _list.push(convNode(iList[rep]));
+    }
+    return _list;
+    console.log(_list);
+  };
+  var _list = parseList(iList || []);
+  console.log(_list);
+
+  var Constructor = function(){};
 
   Constructor.prototype.changeList = function(iList){
-    _list = this.parseList(iList);
+    _list = parseList(iList);
   };
   Constructor.prototype.checkList = function(){
     return _list;
   };
   Constructor.prototype.testNum = function(iNum){
     //precalc Object.keys to increase speed & reduce code
-    console.log(this);
     var tempString = '';
     for(var rep=0;rep < _list.length;rep++){
       if(iNum % _list[rep].key === 0){
